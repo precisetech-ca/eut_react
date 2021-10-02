597eb221d604b6bc165fefd515d5452f77683ee6
Salman Bukhari <salman.bukhari@technologyrivers.com>
2021-10-02 09:33:59 +0500

    po order form ready

--- src/app/pages/purchase/components/EditForm.js
+++ src/app/pages/purchase/components/EditForm.js
@@ -3,8 +3,8 @@
 // Please, be familiar with article first:
 // https://hackernoon.com/react-form-validation-with-formik-and-yup-8b76bda62e10
 import React from "react";
-import { Modal } from "react-bootstrap";
-import { Formik, Form, Field } from "formik";
+import { Modal, Form,Button } from "react-bootstrap";
+import { Formik, Field } from "formik";
 import * as Yup from "yup";
 import {
   Input,
@@ -16,7 +16,7 @@ import { useUIContext } from "../context/UIContext";
 import Select from 'react-select'
 
 // Validation schema
-const InventoryEditSchema = Yup.object().shape({
+const EditFormSchema = Yup.object().shape({
   firstName: Yup.string()
     .min(3, "Minimum 3 symbols")
     .max(50, "Maximum 50 symbols")
@@ -35,20 +35,20 @@ const InventoryEditSchema = Yup.object().shape({
   ipAddress: Yup.string().required("IP Address is required"),
 });
 
-export function InventoryEditForm({
+export function EditForm({
   saveCustomer,
   customer,
   actionsLoading,
   onHide,
 }) {
-  const { warehouseMockData, prefferedSupplier, weightMockProps } = useUIContext();
+  const { warehouseMockData } = useUIContext();
 
   return (
     <>
       <Formik
         enableReinitialize={true}
         initialValues={customer}
-        validationSchema={InventoryEditSchema}
+        validationSchema={EditFormSchema}
         onSubmit={(values) => {
           saveCustomer(values);
         }}
@@ -62,9 +62,22 @@ export function InventoryEditForm({
                 </div>
               )}
               <Form className="form form-label-right">
+                <div className="row">
+                  <div className="col-lg-8"></div>
+                  <div className="col-lg-2 col-sm-1">
+                  <Form.Group className="mb-3" controlId="completed">
+                    <Form.Check type="checkbox" label="Completed" />
+                  </Form.Group>
+                  </div>
+                  <div className="col-lg-2 col-sm-1">
+                    <Form.Group className="mb-3" controlId="void">
+                      <Form.Check type="checkbox" label="Void" />
+                    </Form.Group>
+                  </div>
+                </div>
                 <div className="form-group row">
                   {/* First Name */}
-                  <div className="col-lg-3 col-sm-3">
+                  <div className="col-lg-4 col-sm-3">
                     <Field
                       name="PONumber"
                       component={Input}
@@ -73,87 +86,81 @@ export function InventoryEditForm({
                       className="form-control "
                     />
                   </div>
-                  <div className="col-lg-3 col-sm-3">
+                  <div className="col-lg-4 col-sm-3">
                     <Field
-                      name="PODate"
-                      component={Input}
-                      placeholder="PO Date"
-                      label="PO Date"
-                      className="form-control "
-                    />
-                  </div>
-                  <div className="col-lg-3">
-                    <Field
-                      name="average_cost"
-                      component={Input}
-                      placeholder="Reference"
-                      label="Reference"
-                      className="form-control"
-                    />
-                  </div>
-                  <div className="col-lg-1 col-sm-1">
-                    <FormLabel>Closed</FormLabel>
-                    <FormSwitch />
-                   
-                  </div>
-                  <div className="col-lg-1 col-sm-1">
-                    <FormLabel>Void</FormLabel>
-                    <FormSwitch />
-                  </div>
-                </div>
-                {/* Email */}
-                <div className="form-group row">
-                  
-                  <div className="col-lg-3">
-                    <FormLabel>Supplier</FormLabel>
-                    <Select options={warehouseMockData} />
-                  </div>
-                 
-                  <div className="col-lg-3">
-                    <FormLabel>Warehouse</FormLabel>
-                    <Select options={warehouseMockData} />
-                  </div>
-                  <div className="col-lg-3">
-                    <Field
-                      name="description"
+                      name="phone"
                       component={Input}
                       placeholder="Phone"
                       label="Phone"
                       className="form-control "
                     />
                   </div>
-                  <div className="col-lg-3">
+                  <div className="col-lg-4">
+                    <FormLabel>Prepared By</FormLabel>
+                    <Select options={warehouseMockData} />
+                  </div>
+                  
+                </div>
+                {/* Email */}
+                <div className="form-group row">
+                  <div className="col-lg-4">
                     <Field
-                      name="description"
+                      name="po_date"
+                      component={Input}
+                      placeholder="PO Date / Lead Time"
+                      label="PO Date / Lead Time"
+                      className="form-control "
+                    />
+                  </div>
+
+                  <div className="col-lg-4">
+                    <Field
+                      name="reference"
+                      component={Input}
+                      placeholder="Reference"
+                      label="Reference"
+                      className="form-control "
+                    />
+                  </div>
+                  
+                  <div className="col-lg-4">
+                    <Field
+                      name="fax"
                       component={Input}
                       placeholder="Fax"
                       label="Fax"
                       className="form-control "
                     />
                   </div>
-                  
-                </div>
+
+                </div> 
+
                 <div className="form-group row">
-                   
-                  <div className="col-lg-3">
+                  <div className="col-lg-4">
+                    <FormLabel>Supplier</FormLabel>
+                    <Select options={warehouseMockData} />
+                    <Button className="btn btn-dark mt-2">+ Create</Button>
+                  </div>
+                  <div className="col-lg-4">
                     <Field
-                      name="description"
+                      name="email"
                       component={Input}
                       placeholder="Email"
                       label="Email"
                       className="form-control "
                     />
                   </div>
-                  <div className="col-lg-3">
+                  <div className="col-lg-4">
                     <Field
-                      name="standard_cost"
+                      name="notes"
                       component={Input}
                       placeholder="Notes"
                       label="Notes"
-                      className="form-control form-control-xl"
+                      className="form-control "
                     />
                   </div>
-                </div> 
+                </div>
+                
                 
               </Form>
             </Modal.Body>
