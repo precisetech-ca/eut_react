94190a52016924b129c8efaba91412f0a1cb6625
Salman Bukhari <salman.bukhari@technologyrivers.com>
2021-10-16 23:44:51 +0500

    payload set

--- src/app/pages/inventory/_redux/actions.js
+++ src/app/pages/inventory/_redux/actions.js
@@ -1,4 +1,4 @@
-import { callGenericGetterAsync } from "app/generic/actions";
+import { callGenericGetterAsync, callGenericAsync } from "app/generic/actions";
 import {inventorySlice, callTypes} from "./inventorySlice";
 import { table_data } from "./mock/inventory.mock";
 const {actions} = inventorySlice;
@@ -6,7 +6,24 @@ const {actions} = inventorySlice;
 export const inventoryItemsFetched = queryParams => dispatch => {
   dispatch(actions.startCall({ callType: callTypes.action }));
 
-  dispatch(callGenericGetterAsync('/ItemMaster/GetPartsList', (res) => {
+  const payload = {
+      "data":
+      {  
+        "OFFSET"		 : "+4:00",
+        "ORDER"		 	 : "PAR_ID DESC",
+        "ACTIVE_FLAG"	 : "Y",
+        "RNUM_FROM"	     : "1",
+        "RNUM_TO"		 : "100"
+      }, 
+      "action": "ItemMaster",
+      "method": "GetPartsList",
+      "username": "admin",
+      "password": "admin",
+      "type": "rpc",
+      "tid": "144"
+  };
+
+  dispatch(callGenericAsync(payload, '/ItemMaster/GetPartsList', 'post', (res) => {
     if ( res?.CODE === 'SUCCESS' ) {
       dispatch(actions.inventoriesFetched({
         callType: callTypes.action,
