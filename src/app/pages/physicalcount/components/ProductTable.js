3cc2dcae21b0ee5ba916bd2163fc4510627e6687
Muhammad Zohaib <zohaibkhattak6@gmail.com>
2021-10-27 21:45:24 +0500

    commit on salersorder-dev

---
+++ src/app/pages/physicalcount/components/ProductTable.js
@@ -0,0 +1,200 @@
+// React bootstrap table next =>
+// DOCS: https://react-bootstrap-table.github.io/react-bootstrap-table2/docs/
+// STORYBOOK: https://react-bootstrap-table.github.io/react-bootstrap-table2/storybook/index.html
+import React, { useEffect, useMemo } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory, {
+        PaginationProvider,
+} from "react-bootstrap-table2-paginator";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import * as actions from "../_redux/actions";
+import {
+  getSelectRow,
+  getHandlerTableChange,
+  NoRecordsFoundMessage,
+  PleaseWaitMessage,
+  sortCaret,
+  headerSortingClasses,
+} from "../../../../_metronic/_helpers";
+import * as uiHelpers from "../utils/UIHelpers";
+import * as columnFormatters from "../column-formatter";
+import { Pagination } from "../../../../_metronic/_partials/controls";
+import { usePartsReturnUIContext } from "../context/PhysicalCountUIContext";
+
+export function Table() {
+  // Customers UI Context
+  const PartsReturnUIContext = usePartsReturnUIContext();
+  const customersUIProps = useMemo(() => {
+    return {
+      ids: PartsReturnUIContext.ids,
+      setIds: PartsReturnUIContext.setIds,
+      queryParams: PartsReturnUIContext.queryParams,
+      setQueryParams: PartsReturnUIContext.setQueryParams,
+      openEditCustomerDialog: PartsReturnUIContext.openEditCustomerDialog,
+      openDeleteCustomerDialog: PartsReturnUIContext.openDeleteCustomerDialog,
+    };
+  }, [PartsReturnUIContext]);
+
+  // Getting curret state of customers list from store (Redux)
+  const { currentState } = useSelector(
+    (state) => ({ currentState: state.customers }),
+    shallowEqual
+  );
+  const { totalCount, entities, listLoading } = currentState;
+
+  // Customers Redux state
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // clear selections list
+    customersUIProps.setIds([]);
+    // server call by queryParams
+    dispatch(actions.fetchCustomers(customersUIProps.queryParams));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [customersUIProps.queryParams, dispatch]);
+  // Table columns
+  const columns = [
+    {
+      dataField: "id",
+      text: "ID",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "firstName",
+      text: "SKU",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "lastName",
+      text: "Barcode",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "email",
+      text: "Desc",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "gender",
+      text: "Lot #",
+      sort: false,
+      sortCaret: sortCaret,
+    },
+    {
+      dataField: "status",
+      text: "Expirey",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "type",
+      text: "OH qty",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+      dataField: "type",
+      text: "AVT qty",
+      sort: true,
+      sortCaret: sortCaret,
+      headerSortingClasses,
+    },
+    {
+        dataField: "type",
+        text: "Order qty",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      },
+      {
+        dataField: "type",
+        text: "UoM",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      },
+      {
+        dataField: "type",
+        text: "Cost",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      },
+      {
+        dataField: "type",
+        text: "Tax",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      },
+      {
+        dataField: "type",
+        text: "Last Cost",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      },
+      {
+        dataField: "type",
+        text: "Sub Total",
+        sort: true,
+        sortCaret: sortCaret,
+        headerSortingClasses,
+      }
+  ];
+  // Table pagination properties
+  const paginationOptions = {
+    custom: true,
+    totalSize: totalCount,
+    sizePerPageList: uiHelpers.sizePerPageList,
+    sizePerPage: customersUIProps.queryParams.pageSize,
+    page: customersUIProps.queryParams.pageNumber,
+  };
+  return (
+    <>
+      <PaginationProvider pagination={paginationFactory(paginationOptions)}>
+        {({ paginationProps, paginationTableProps }) => {
+          return (
+            <Pagination
+              isLoading={listLoading}
+              paginationProps={paginationProps}
+            >
+              <BootstrapTable
+                wrapperClasses="table-responsive"
+                bordered={false}
+                classes="table table-head-custom table-vertical-center overflow-hidden"
+                bootstrap4
+                remote
+                keyField="id"
+                data={entities === null ? [] : entities}
+                columns={columns}
+                defaultSorted={uiHelpers.defaultSorted}
+                onTableChange={getHandlerTableChange(
+                  customersUIProps.setQueryParams
+                )}
+                selectRow={getSelectRow({
+                  entities,
+                  ids: customersUIProps.ids,
+                  setIds: customersUIProps.setIds,
+                })}
+                {...paginationTableProps}
+              >
+                <PleaseWaitMessage entities={entities} />
+                <NoRecordsFoundMessage entities={entities} />
+              </BootstrapTable>
+            </Pagination>
+          );
+        }}
+      </PaginationProvider>
+    </>
+  );
+}
